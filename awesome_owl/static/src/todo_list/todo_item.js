94b21166a6e7e7c9308c8e414592ba5bc0dc2c7a
Brian <chumobrian@gmail.com>
2025-05-29 23:52:12 +0300

    feat: add TodoItem component with props validation

---
+++ awesome_owl/static/src/todo_list/todo_item.js
@@ -0,0 +1,7 @@
+/** @odoo-module **/
+import { Component } from "@odoo/owl";
+
+export class TodoItem extends Component {
+    static template = "awesome_owl.todo_item";
+    static props = { todo: Object };
+}
