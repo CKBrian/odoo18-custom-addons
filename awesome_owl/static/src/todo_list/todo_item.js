fb17c1a3842fd9b27ff70fcf268dfa00fca2d8b7
Brian <chumobrian@gmail.com>
2025-05-31 09:23:40 +0300

    feat(todo_item): add toggle and delete handlers using props

--- awesome_owl/static/src/todo_list/todo_item.js
+++ awesome_owl/static/src/todo_list/todo_item.js
@@ -3,5 +3,20 @@ import { Component } from "@odoo/owl";
 
 export class TodoItem extends Component {
     static template = "awesome_owl.todo_item";
-    static props = { todo: Object };
+    static props = {
+        todo: Object,
+        toggleState: { type: Function, optional: true },
+        removeTodo: { type: Function, optional: true },
+    };
+
+    toggle() {
+        if (this.props.toggleState) {
+            this.props.toggleState(this.props.todo.id);
+        }
+    }
+    deleteTodo() {
+        if (this.props.removeTodo) {
+            this.props.removeTodo(this.props.todo.id);
+        }
+    }
 }
