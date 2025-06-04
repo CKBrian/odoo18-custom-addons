eec732ba15bf20656d280afbcfd6922a4225cc98
Brian <chumobrian@gmail.com>
2025-06-04 16:44:28 +0300

    feat(card): add slot support and toggle state with useState

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,9 +1,12 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
 	static props = {
-		title: String,
-		content: String,
+		title: { type: String, optional: false },
+		slots: true,
 	};
 	static template = "awesome_owl.card";
+	setup() {
+		this.cardState = useState({ isOpen: true });
+	}
 }
